46032af0352670a41233e3c03297dbbc70fede7e
elsetiyawan <el.setiyawan@gmail.com>
2020-09-16 08:46:48 +0700

    loging

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-const io = require('socket.io')(3000)
+const io = require("socket.io")(3000);
+const fs = require("fs");
+const util = require("util");
+const log_file = fs.createWriteStream(__dirname + "/debug.log", { flags: "w" });
+const log_stdout = process.stdout;
 
-const users = {}
+const users = {};
 
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+  socket.on("send-chat-message", (message) => {
+    const date = new Date();
+    const text = `${date} - ${users[socket.id]} : ${message}`;
+    console.log(text);
+    log_file.write(util.format(text) + "\n");
+    log_stdout.write(util.format(text) + "\n");
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
